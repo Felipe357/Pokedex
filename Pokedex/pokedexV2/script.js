6bc3c7d644396b796a429ab0ece63089ce6c5856
Felipe357 <felipeserra.100@gmail.com>
2022-11-08 11:20:50 -0300

    Listando na ordem

--- Pokedex/pokedexV2/script.js
+++ Pokedex/pokedexV2/script.js
@@ -1,5 +1,5 @@
 function carregar() {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=105&offset=0")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=9&offset=0")
         .then((response) => {
             return response.json();
         })
@@ -7,28 +7,27 @@ function carregar() {
 
             data.results.forEach((e, indice) => {
 
-                var poke = document.querySelector(".pokemon")
-                var pokemon = poke.cloneNode(true)
-                pokemon.classList.remove("model")
-
-                pokemon.querySelector("#name").innerHTML = e.name
-                
-
-
                 fetch(e.url)
                     .then(res => { return res.json() })
                     .then(y => {
 
-                        pokemon.querySelector("#id").innerHTML = "#"+y.id
+                        var poke = document.querySelector(".pokemon")
+                        var pokemon = poke.cloneNode(true)
+                        pokemon.classList.remove("model")
+                        pokemon.id = y.id
 
-                        pokemon.querySelector("#pokeImg").style.backgroundImage = "url("+y.sprites.front_default+")"
+                        pokemon.querySelector("#name").innerHTML = e.name
+
+                        pokemon.querySelector("#id").innerHTML = "#" + y.id
+
+                        pokemon.querySelector("#pokeImg").style.backgroundImage = "url(" + y.sprites.front_default + ")"
 
                         y.types.forEach((z, indice) => {
 
                             var type = pokemon.querySelector(".type")
                             type.classList.remove("model")
 
-                            if(indice == 0){
+                            if (indice == 0) {
                                 type.querySelector("span").innerHTML = z.type.name
                                 pokemon.style.backgroundColor = corPoke(z.type.name);
                             }
@@ -46,15 +45,37 @@ const corPoke = (type) => {
     switch (type) {
 
         case 'fire':
-            return('#FFA860')
+            return ('#FFA860')
 
         case 'water':
-            return('#37A7F2')
+            return ('#37A7F2')
 
         case 'grass':
-            return('#8dc797')
+            return ('#8dc797')
 
         default:
-            return("#efefef")
+            return ("#efefef")
     }
 }
+
+
+setTimeout(() => {
+    listarOrdem()
+}, 500)
+
+async function listarOrdem() {
+    var all = Array.from(document.querySelectorAll(".pokemon"))
+
+    
+
+    all.sort((a,b) => a.id - b.id);
+
+    all.forEach((s) => {
+        var poke = s
+        document.querySelector("main").appendChild(poke)
+    })
+}
+
+// listarOrdem().then(
+//     mostrar()
+// )
